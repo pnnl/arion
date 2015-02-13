2215bdf2ce4b99c66a6f8174e9950dece388ed53
Peter Nordquist <peter.nordquist@pnnl.gov>
2015-02-13 00:46:07 +0000

    Write prosser.glm file

--- src/test/java/ExperimentFncsTest.java
+++ src/test/java/ExperimentFncsTest.java
@@ -1,3 +1,4 @@
+import gov.pnnl.prosser.api.GldSimulatorWriter;
 import gov.pnnl.prosser.api.Ns3SimulatorWriter;
 import gov.pnnl.prosser.api.gld.obj.AuctionObject;
 import gov.pnnl.prosser.api.pwr.obj.Controller;
@@ -22,6 +23,7 @@ public class ExperimentFncsTest {
     public static void main(final String[] args) throws IOException {
         final Path outPath = Paths.get(args[0]).toRealPath();
         final GldBaseFncsTest gldSim = new GldBaseFncsTest();
+        GldSimulatorWriter.writeGldSimulator(outPath.resolve("prosser.glm"), gldSim);
 
         // For this test there will be one object in each list but in general there could be multiple
         final List<AuctionObject> auctions = new ArrayList<>();
